400dca0d1a0f0a10f819ce52085c44c8a91a2c35
henrique.bassi <henrique.bassi@tecnospeed.com.br>
2020-08-23 20:59:20 -0300

    Ajustando port para Heroku

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-    console.log('server started on port 3333!');
+
+app.listen(process.env.PORT || 3333, () => {
+    console.log('server started');
 });
